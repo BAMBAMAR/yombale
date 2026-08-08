2fc826db7925f1c141ea71c33ef3644080f91690
BAMBAMAR <pastefprojetbi@gmail.com>
2026-08-08 00:37:30 +0000

    fix(build): resolution complete des erreurs de syntaxe et validation build standalone

--- frontend-next/src/components/TrackingPixels.tsx
+++ frontend-next/src/components/TrackingPixels.tsx
@@ -23,7 +23,7 @@ export default function TrackingPixels({ metaPixelId, tiktokPixelId, ga4Id }: Tr
       if (!window.fbq) {
         /* eslint-disable */
         // @ts-ignore
-        (function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        ;(function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
         n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
         n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
         t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)})
@@ -41,7 +41,7 @@ export default function TrackingPixels({ metaPixelId, tiktokPixelId, ga4Id }: Tr
       if (!window.ttq) {
         /* eslint-disable */
         // @ts-ignore
-        (function(w,d,t){w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
+        ;(function(w,d,t){w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
         ttq.load(tiktokPixelId);
         ttq.page();
         /* eslint-enable */
@@ -63,7 +63,7 @@ export default function TrackingPixels({ metaPixelId, tiktokPixelId, ga4Id }: Tr
           // eslint-disable-next-line prefer-rest-params
           window.dataLayer!.push(arguments as unknown as Record<string, unknown>)
         }
-        window.gtag('js', 'new Date()')
+        window.gtag('js', new Date() as unknown as string)
         window.gtag('config', ga4Id)
       }
     }
